0987b19822b19292031e8a880097c1ec68d42a7f
jinglei <965334541@qq.com>
2020-06-28 17:37:47 +0800

    Initial commit

---
+++ titlestatusbar/src/main/java/com/csnt/titlestatusbar/Util.java
@@ -0,0 +1,13 @@
+package com.csnt.titlestatusbar;
+
+import android.text.TextUtils;
+
+/**
+ * Created by sunrain
+ * Created Date 2020/6/28 5:33 PM
+ */
+public class Util {
+    public static boolean isEmpty(String str){
+        return TextUtils.isEmpty(str);
+    }
+}
