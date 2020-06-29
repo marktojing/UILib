fb5c51b763b60c08a864bf9c9e9cb82c1725861a
jinglei <965334541@qq.com>
2020-06-29 15:08:44 +0800

    删除switchButton相关信息push到git

--- titlestatusbar/src/main/java/com/csnt/titlestatusbar/viewUtils/ColorUtil.java
+++ titlestatusbar/src/main/java/com/csnt/titlestatusbar/viewUtils/ColorUtil.java
@@ -5,6 +5,7 @@ package com.csnt.titlestatusbar.viewUtils;
  * Created Date 2020/6/9 12:02 AM
  */
 public class ColorUtil {
+
     public static int setAlpha(int percent,int color){
         return ((255*percent/100)<<24)|color;
     }
